485d92223875da3be1ef21ee1520b51201ebdfd1
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-05-15 09:00:49 +0200

    Refactor the cli and npm containers

--- PHP/Laravel/Scripts/composer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker run -it --rm -v $PWD:$PWD -w $PWD composer "$@"
-# curl -L -O  https://github.com/laravel/laravel/archive/v"$1".tar.gz
-# tar -zxvf v"$1".tar.gz
-# rm v"$1".tar.gz
--- PHP/Laravel/Scripts/phpunit-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clear
-vendor/bin/phpunit $@
